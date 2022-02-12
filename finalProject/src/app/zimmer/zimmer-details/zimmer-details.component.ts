7d37c1dcce62402510822842334fdddb8faa94ef
Tal Randi <talrandi001@gmail.com>
2022-02-12 16:18:27 +0200

    Added zimmer-details page

--- finalProject/src/app/zimmer/zimmer-details/zimmer-details.component.ts
+++ finalProject/src/app/zimmer/zimmer-details/zimmer-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataStorageService } from 'src/app/shared-data/data-storage.service';
+import { Zimmer } from 'src/app/shared-data/zimmer.model';
 
 @Component({
   selector: 'app-zimmer-details',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZimmerDetailsComponent implements OnInit {
 
-  constructor() { }
+  isLoading = false;
+  zimmer_id: string;
+  zimmer: Zimmer;
+
+  constructor(private storage: DataStorageService, private router: Router) { }
 
   ngOnInit(): void {
+    this.isLoading = true
+    this.zimmer_id = this.router.url.split('/').pop()!
+      
+    this.storage.fetchAcceptedZimmers().subscribe(zimmers => {
+    
+      this.zimmer = zimmers.filter(zimmer => {
+        return zimmer.zimmer_id == this.zimmer_id
+      })[0]
+      this.isLoading = false
+      if(this.zimmer === undefined)
+        this.router.navigate(['/not-found']);
+    })
+  
+
+    
+
   }
 
 }
